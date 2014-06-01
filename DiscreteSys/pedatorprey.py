f24909e4a3457368c4464fa597f9d28f20e2d4d3
Steven Broad <sebroad@yahoo.com>
2014-06-01 08:06:36 -0400

    Revisions to predator-prey

--- DiscreteSys/pedatorprey.py
+++ DiscreteSys/pedatorprey.py
@@ -5,20 +5,54 @@ Created on Mon May 19 14:00:12 2014
 @author: sbroad
 """
 
+import numpy as np
 import matplotlib.pyplot as plt
 
-def predprey(x,y,a,b=0.001,c=0.01,d=0.01):
-    xnew = a*x - b*y*x
-    ynew = -c*y + d*x*y
-    return (max(xnew,0),max(ynew,0))
+def predprey(x,y,a,b,c,d,t=1):
+    """
+    This is an update function for the predator-prey model.
+    x = prey population
+    y = predator population
+    delta x = (a*x - b*x*y)*t
+    delta y = (-c*y + d*x*y)*t
+    """
+    for step in range(1/t):
+        x += x * (a - b*y) * t
+        y += y * (d*x - c) * t
+    return (x,y)
 
-x = 100
-y = 50
-a = 1.10081
+# setup parameters
+x = 130
+y = 20
+a = 0.1
+b = 0.002
+c = 0.1
+d = 0.001
 
-plt.close()
-for i in range(200):
-    print x,y
-    plt.plot(i,x,'ro-')
-    plt.plot(i,y,'bo-')
-    (x,y) = predprey(x,y,a)
+# setup prey and pred lists
+prey = []
+pred = []
+step = range(2000)
+for i in step:
+    prey.append(x)
+    pred.append(y)
+    (x,y) = predprey(x,y,a,b,c,d)
+
+# Plot a time graph
+plt.close(1)
+plt.figure(1)
+plt.title('Prey and Predator populations vs. time')
+plt.xlabel('Time periods')
+plt.ylabel('Populations')
+plt.plot(step, prey, 'b-+', label='Prey')
+plt.plot(step,pred,'r-o',label="Predator")
+plt.legend(loc='best')
+
+# Plot a phase graph
+#plt.close(2)
+plt.figure(2)
+plt.title('Predator vs. Prey population')
+plt.xlabel('Prey population')
+plt.ylabel('Predator population')
+plt.plot(prey,pred,'-o',label='V=%.2f' % (-d*x+c*np.log10(x)+b*y+a*log10(y)))
+plt.legend(loc='best')
