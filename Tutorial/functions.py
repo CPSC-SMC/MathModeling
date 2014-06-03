dbef4b8c118e519a9a2e34c030a81ae9f33f226e
javaprofsmc <sbroad@saintmarys.edu>
2014-06-03 16:01:00 -0400

    Revise function tutorial

--- Tutorial/functions.py
+++ Tutorial/functions.py
@@ -5,29 +5,40 @@
 # 0. import numpy and pyplot
 
 
-# 1. Define a function that returns the exponential of sqrt x
+
+# 1. Create an array X of values between 0 and 2 with numpy.linspace(...). Use num=21.
 
 
 
-# 2. Define another function that returns m*x+b. Make b and m the 2nd and 3rd inputs to the function
+
+# 2. Define a function that returns m*x+b. Make b and m the 2nd and 3rd inputs to the function
 
 
 
-# 3. Create an array X of values between 0 and 2 with numpy.linspace(...). Use num=21.
+
+# 3. Plot this line. Use a slope of 0.5 and an intercept of -1
 
 
 
-# 4. Plot each of these functions. Use a slope of 0.5 and an intercept of -1
+
+# 4. Define a function that returns the exponential of sqrt x
 
 
 
-# 5. Define a piecewise function which has three pieces:
+
+# 5. Plot this curve 
+
+
+
+
+
+# 6. Define a piecewise function which has three pieces:
 #       cos(x) when x < pi
 #       (x-pi)**2 - 1 when pi < x < 5
 #       log(x) when x >=5
 
 
-# 6. Use list comprehension to create the y-values
+# 7. Use list comprehension to create the y-values
 
 
-# 7. plot the piecewise function
+# 8. plot the piecewise function
