48abd998f7d7f9d4401e806edbae9726d84fcd10
javaprofsmc <sbroad@saintmarys.edu>
2014-08-22 12:04:50 -0400

    Exponential least squares

--- DataFitting/exponential.py
+++ DataFitting/exponential.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Quadratic least squares
+Exponential least squares
 
 Created on Fri Aug 22 11:41:12 2014
 
@@ -17,16 +17,20 @@ y = np.array([0.12, 0.24, 0.72, 1.30, 1.90])
 # plot this data
 plt.plot(x, y, '+')
 
-# fit the data
-p = np.polyfit(x, y, 2) # the two is the degree of the polynomial
+# compute the log of all y values
+logy = np.log(y)
+
+# linear fit the logy data to the x data
+p = np.polyfit(x, logy, 1) # the one is the degree of the polynomial
 print p
 
 # make a function from the polynomial fit
-f = np.poly1d(p)
+logf = np.poly1d(p)
 
 # plot the fit function
 X = np.linspace(-1, 5, 21) # a range of values that encompass all x data
-plt.plot(X, f(X),'--')
+# the fit is logarithmic so we must compose the exponential function
+plt.plot(X, np.exp(logf(X)),'--')
 
 # plot the fits for the original data points
-plt.plot(x, f(x), 'o')
+plt.plot(x, np.exp(logf(x)), 'o')
